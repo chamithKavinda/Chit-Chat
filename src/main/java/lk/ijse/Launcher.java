2ff470d9b6c0601e8a93365ad71d147948d9eeae
chamithKavinda <chamith13kavinda@gmail.com>
2024-01-23 03:10:36 +0530

    Login Form design

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class Launcher {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    @FXML
+    private AnchorPane LoginPage;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
